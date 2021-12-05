c387865bd81cbf799dc960a76c2cb1b4cdea4658
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-05 17:01:34 +0100

    Finished day 1.

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,6 +1,34 @@
-pub fn part1() -> String {
-    String::new()
+use aoc_lib::load_to_rows;
+
+pub fn part1() -> i32 {
+    let i = load_to_rows("input/day1.txt")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    for a in &i {
+        for b in &i {
+            if a + b == 2020 {
+                return a * b;
+            }
+        }
+    }
+    0
 }
-pub fn part2() -> String {
-    String::new()
+pub fn part2() -> i32 {
+    let i = load_to_rows("input/day1.txt")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    for a in &i {
+        for b in &i {
+            for c in &i {
+                if a + b + c == 2020 {
+                    return a * b * c;
+                }
+            }
+        }
+    }
+    0
 }
