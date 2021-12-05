c84a55f20d64150f4812f100893ba909ee3ed500
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2021-12-05 17:22:46 +0100

    Finished day 2.

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,6 +1,43 @@
-pub fn part1() -> String {
-    String::new()
+use aoc_lib::load_to_rows;
+
+pub fn part1() -> usize {
+    load_to_rows("input/day2.txt")
+        .filter(|s| !s.is_empty())
+        .filter(|s| {
+            let mut s = s.split(' ');
+            let (a, b) = {
+                let mut nums = s.next().unwrap().split('-');
+                (
+                    nums.next().unwrap().parse::<usize>().unwrap(),
+                    nums.next().unwrap().parse::<usize>().unwrap(),
+                )
+            };
+            let letter = s.next().unwrap().chars().next().unwrap();
+            let pass = s.next().unwrap();
+
+            let count = pass.chars().filter(|c| *c == letter).count();
+            a <= count && count <= b
+        })
+        .count()
 }
-pub fn part2() -> String {
-    String::new()
+pub fn part2() -> usize {
+    load_to_rows("input/day2.txt")
+        .filter(|s| !s.is_empty())
+        .filter(|s| {
+            let mut s = s.split(' ');
+            let (a, b) = {
+                let mut nums = s.next().unwrap().split('-');
+                (
+                    nums.next().unwrap().parse::<usize>().unwrap() - 1,
+                    nums.next().unwrap().parse::<usize>().unwrap() - 1,
+                )
+            };
+            let letter = s.next().unwrap().chars().next().unwrap();
+            let pass = s.next().unwrap();
+
+            let a = pass.chars().nth(a).unwrap();
+            let b = pass.chars().nth(b).unwrap();
+            (a == letter && b != letter) || (a != letter && b == letter)
+        })
+        .count()
 }
